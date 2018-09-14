967a8d816ebf280d711ea791ed7ac1862f90616c
Andrei Serban <aiserban@outlook.com>
2018-09-14 08:38:26 +0300

    added basic resource: wood

--- script/engine.js
+++ script/engine.js
@@ -3,32 +3,54 @@ window.Game.currentTick = 0;
 window.Game.tickSpeed = 500;  // tick speed in milliseconds
 window.Game.startDateTime = new Date().toString();
 window.Game.running = true;
+window.Game.resources = {};
+window.Game.resources.wood = 0;
 
 window.Game.Update = function() {
-    function updateTickCounter() {
-        window.Game.currentTick += 1;
-        document.getElementById('tickCounter').innerHTML = window.Game.currentTick;
-        console.log(window.Game.currentTick);
-    }
-
-    function eventCheck(){
-        window.Game.Events.forEach(event => {
-            if(event.isAvailable()){
-                event.trigger();
-            }
-        });
-    }
-
-    updateTickCounter();
-    eventCheck();
+    window.Game.updateTickCounter();
+    window.Game.updateResources();
+    window.Game.eventCheck();
 }
 
+
+/**
+ * Checks for and triggers an available event
+ *
+ */
+window.Game.eventCheck = function(){
+    window.Game.Events.forEach(event => {
+        if(event.isAvailable()){
+            event.trigger();
+        }
+    });
+}
+
+/**
+ * Increases the game counter and displays the value in the UI
+ *
+ */
+window.Game.updateTickCounter = function() {
+    window.Game.currentTick += 1;
+    document.getElementById('tickCounter').innerHTML = window.Game.currentTick;
+    console.log(window.Game.currentTick);
+}
+
+
+/**
+ * Updates the resource count in the UI
+ *
+ */
+window.Game.updateResources = function(){
+    document.getElementById('wood').innerHTML = window.Game.resources.wood;
+}
+
+window.Game.gatherWood = function(){
+    window.Game.resources.wood += 1;
+    window.Game.updateResources();
+}
 
 window.Game.MainLoop = function() {
     setInterval(window.Game.Update, window.Game.tickSpeed);
 }
 
-if (window.Game.running) {
-
-}
 window.Game.MainLoop();
